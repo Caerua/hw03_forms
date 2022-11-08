bcd5937f89b132cd65303f2a8f4756951056294b
Арина Огурцова <ogurtsovarina@mail.ru>
2022-11-08 22:32:04 +0300

    правки2

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -4,5 +4,5 @@ from django.utils import timezone
 def year(request):
     """Добавляет переменную с текущим годом."""
     return {
-        'year': int(str(timezone.localtime(timezone.now()))[:4])
+        'year': int(str(timezone.now().year))
     }
